bd6a42b7a59556be02a96e809fdaa5c3001dddf1
= <=>
2014-12-04 21:18:16 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pushchat::Application.config.secret_token = '836a155f73dfaf090866f19ccb694afe2e4de525b932eb7b4a028995e6be4e43028e5bad32a89174e2e853d37f244d49968d03faa00dfb53857cf449c4f95ba3'

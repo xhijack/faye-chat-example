6191000a60165edea86ac3181e0b1a740057543a
ramdani <xhijack@gmail.com>
2014-12-25 17:51:40 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Pushchat::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'messages#list'
+   root :to => 'messages#index'
 
   # See how all your routes lay out with "rake routes"
 
